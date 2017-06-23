b0243ac5b412ce6f6817854edad3a986ef3fc1b6
nia-427 <ns0042@mix.wvu.edu>
2017-06-23 15:47:52 -0400

    New names

---
+++ Names.rb
@@ -0,0 +1,17 @@
+def set
+	puts "Your name is: "
+end
+
+def firstName(first)
+p first
+end
+
+def lastName(last)
+p last
+end
+
+1.times do
+	set
+	firstName("Nima")
+	lastName("ShahabShahmir")
+end
---
+++ Names2.rb
@@ -0,0 +1,7 @@
+name = "Nima"
+lastname = "ShahabShahmir"
+fullname = name + "\s" + lastname
+
+puts "Your name is #{name}."
+puts "Your last name is #{lastname}."
+puts "Your full name is #{fullname}."
